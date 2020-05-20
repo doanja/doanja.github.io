6b0ede517829c162eb30586b07b042efc53f0c54
John Doan <doanja@plu.edu>
2020-05-20 11:28:51 -0700

    updated projects

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,6 +4,7 @@ const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
+const main = document.getElementById('home');
 
 const toggleMenu = () => {
   if (!showMenu) {
@@ -11,7 +12,7 @@ const toggleMenu = () => {
     menu.classList.add('show');
     menuNav.classList.add('show');
     menuBranding.classList.add('show');
-    navItems.forEach(item => item.classList.add('show'));
+    navItems.forEach((item) => item.classList.add('show'));
 
     // set menu state
     showMenu = true;
@@ -20,7 +21,7 @@ const toggleMenu = () => {
     menu.classList.remove('show');
     menuNav.classList.remove('show');
     menuBranding.classList.remove('show');
-    navItems.forEach(item => item.classList.remove('show'));
+    navItems.forEach((item) => item.classList.remove('show'));
 
     // set menu state
     showMenu = false;
@@ -30,3 +31,15 @@ const toggleMenu = () => {
 // set initial state of the menu
 let showMenu = false;
 menuBtn.addEventListener('click', toggleMenu);
+
+const test = () => {
+  alert('home clicked');
+};
+
+main.addEventListener('click', test);
+
+const test2 = () => {
+  alert('menu clicked');
+};
+
+menuNav.addEventListener('click', test2);
