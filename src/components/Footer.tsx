a265386d38dba19c051559b0edd3c7a05665517a
doanja <doanja@plu.edu>
2021-08-02 15:23:54 -0700

    wip footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,28 @@
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
+
 const Footer = () => {
-  return <footer className='flex justify-center items-center w-full py-8 border-t-2 bg-gray-100'>Copyright &copy; 2021 doanja</footer>;
+  return (
+    <footer className='flex w-full pt-8 pb-12 py-10 bg-gray-100'>
+      <div className='flex flex-row'>
+        <div className='flex flex-col'>
+          <p>Phone</p>
+          <p>123-123-1234</p>
+        </div>
+
+        <div className='flex flex-col'>
+          <p>Phone</p>
+          <p>123-123-1234</p>
+        </div>
+      </div>
+
+      <div className='flex flex-row'>
+        <AiFillGithub size='3rem' />
+        <AiFillLinkedin size='3rem' />
+      </div>
+
+      {/* Copyright &copy; 2021 doanja */}
+    </footer>
+  );
 };
 
 export default Footer;
